4562382d8b38f98d5555f9f0eda9efd9dbe11ab6
Mehrab <mehrab.rahman@revature.com>
2019-04-29 16:17:15 -0500

    First Commit

---
+++ src/main/java/com/revature/servlets/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>Hello World!</h1>");
+		out.close();
+	}
+}
